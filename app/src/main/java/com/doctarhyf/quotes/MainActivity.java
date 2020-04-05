404088da92e0c125c5ad3088a547a0595e402c39
Docta Rhyf <drrhyf@gmail.com>
2020-04-05 11:04:27 +0200

    firestore 101

--- app/src/main/java/com/doctarhyf/quotes/MainActivity.java
+++ app/src/main/java/com/doctarhyf/quotes/MainActivity.java
@@ -1,17 +1,40 @@
 package com.doctarhyf.quotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String QUOTE_KEY = "quote";
+    private static final String AUTHOR_KEY = "author";
+    private static final String TAG = "firestore";
+    Button btnSaveQuote, btnFetchQuote;
 
-    EditText etQuote, etAuthor;
-    Button btnSaveQuote, btnReadQuote;
+    TextView tvQuote;
+
+    private DocumentReference mDocRef =
+            FirebaseFirestore.getInstance().document("sampleData/inspiration");
 
 
     @Override
@@ -20,23 +43,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        etQuote = findViewById(R.id.etQuote);
-        etAuthor = findViewById(R.id.etAuthor);
 
-        btnReadQuote = findViewById(R.id.btnReadQuote);
+        btnFetchQuote = findViewById(R.id.btnFetchQuote);
         btnSaveQuote = findViewById(R.id.btnSaveQuote);
+        tvQuote = findViewById(R.id.tvQuote);
 
-        btnSaveQuote.setOnClickListener(new View.OnClickListener() {
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDocRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View v) {
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(documentSnapshot.exists()){
+                    String quoteText = documentSnapshot.getString(QUOTE_KEY);
+                    String authorText = documentSnapshot.getString(AUTHOR_KEY);
+                    tvQuote.setText("\"" + quoteText + "\" -- " + authorText);
+                }else if(e != null){
+                    Log.w(TAG, "onEvent: exception", e);
+                }
+            }
+        });
+    }
 
+    public void saveQuote(View view) {
+        EditText quoteView = findViewById(R.id.etQuote);
+        EditText authorView = findViewById(R.id.etAuthor);
+        String quoteText = quoteView.getText().toString();
+        String authorText = authorView.getText().toString();
+
+
+        if(quoteText.isEmpty() || authorText.isEmpty()) { return;}
+
+        Map<String, Object> dataToSave = new HashMap<>();
+        dataToSave.put(QUOTE_KEY, quoteText);
+        dataToSave.put(AUTHOR_KEY, authorText);
+        
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.e(TAG, "Document has been saved " );
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Document was not saved " );
             }
         });
 
-        btnReadQuote.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void fetchQuote(View view) {
+
+        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View v) {
-                
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    String quoteText = documentSnapshot.getString(QUOTE_KEY);
+                    String authorText = documentSnapshot.getString(AUTHOR_KEY);
+                    tvQuote.setText("\"" + quoteText + "\" -- " + authorText);
+                }
             }
         });
     }
